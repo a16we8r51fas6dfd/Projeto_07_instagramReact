ca48b5be66d84eb625248369654e1a096d1684fe
fernandinho <fernando_yudi_yaeda@hotmail.com>
2021-12-02 21:24:32 -0300

    componentização ok (eu acho)

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from "./app/Navbar"
+import Corpo from "./app/Corpo"
+import FundoMobile from "./app/FundoMobile"
+
+export default function App() {
+    return (
+      <div>
+        <Navbar />
+        <Corpo />
+        <FundoMobile />
+      </div>
+    )
+}
---
+++ src/app/Corpo.js
@@ -0,0 +1,17 @@
+import Stories from "../corpo/Stories"
+import Posts from "../corpo/Posts"
+import Sidebar from "../corpo/Sidebar"
+
+export default function Corpo() {
+    return (
+        <div class="corpo">
+        <div class="esquerda">
+            <Stories />
+            <Posts />
+        </div>
+
+        <Sidebar />
+        
+        </div>
+        )
+}
